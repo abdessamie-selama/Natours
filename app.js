2882ab1cf4ccb05b3cf70a6b3007e3c66ac3b2ef
sabdesamie <abdesamieselama@gmail.com>
2021-07-26 23:31:03 +0100

    rendering pages & authentication system

--- app.js
+++ app.js
@@ -1,43 +1,87 @@
-const express = require("express")
-const fs = require("fs");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
 const app = express();
 
-app.use(express.json);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-const tours = JSON.parse(
-	fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+// 1) GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
 );
 
-app.get("/api/v1/tours",(req,res)=>{
-
-	res.status(200).json({
-		status:'success',
-		results:tours.length,
-		data:{
-			tours: tours
-		}
-	});
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
+  next();
 });
 
-app.post('/api/v1/tours',(req,res)=>{
-	const newId = tours[tours.length - 1].id + 1;
-	const newTour = Object.assign({id:newId},req.body);
+// 3) ROUTES
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
-	tours.push(newTour);
-
-	fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err => {
-
-		res.status(201).json({
-		status:'success',
-		data:{
-			tour:newTour
-		}
-		});
-	});
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-const port = 3000;
-app.listen(port, ()=>{
-	console.log(`App running on port ${port} ...`);
-});
+app.use(globalErrorHandler);
 
+module.exports = app;
